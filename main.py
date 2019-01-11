d9e5a5fba1a90a1280c02ba2223c24fa0a36f467
Alex Li <tianyi7@ualberta.ca>
2019-01-10 23:11:11 -0700

    fixed main.py so it doesn't save to disk

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ import requests
 
 print("requests version: " + requests.__version__)
 response = requests.get("https://raw.githubusercontent.com/skywolff/CMPUT404-lab01/master/main.py")
-with open("downloadededScript.py", "w") as f:
-    f.write(response.text)
 print(response.text)
